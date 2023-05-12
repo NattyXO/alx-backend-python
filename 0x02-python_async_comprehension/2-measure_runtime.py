d934f77ecbc4ec3303eed5dafbc5d9fdc7cdd168
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-05-12 21:21:35 +0300

    back to origin 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,24 +4,18 @@
 import asyncio
 import random
 import timeit
-from typing import List
-
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime(loop: asyncio.AbstractEventLoop) -> float:
-    """ Executes comprehension func 4 times in parallel, returns runtime """
+async def measure_runtime() -> float:
+    """ executes comprehension func 4 times in parallel, returns runtime """
+
     start = timeit.default_timer()
-    coroutines: List[asyncio.Task] = [async_comprehension() for _ in range(4)]
-    await asyncio.gather(*coroutines, loop=loop)
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+        )
     stop = timeit.default_timer()
     return stop - start
-
-
-async def main() -> float:
-    async with asyncio.get_event_loop() as loop:
-        return await measure_runtime(loop)
-
-
-if __name__ == "__main__":
-    print(asyncio.run(main()))
