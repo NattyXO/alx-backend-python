fb85ef6ffe8269ae6d6ded80e1bd741e61e167d8
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-05-21 23:03:22 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python3
 """ Unittest module """
 
-from unittest import TestCase, mock
-from unittest.mock import patch
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
 
 import client
 from client import GithubOrgClient
 
 class TestGithubOrgClient(TestCase):
-    @patch('client.get_json')
-    def test_public_repos(self, mock_get_json):
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
         org_name = "example"
-        payload = [
-            {"name": "repo1"},
-            {"name": "repo2"},
-            {"name": "repo3"}
-        ]
-        expected_repos = [repo["name"] for repo in payload]
-
-        mock_get_json.return_value = payload
-
-        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=mock.PropertyMock) as mock_public_repos_url:
+        with patch.object(GithubOrgClient, 'public_repos', return_value=[repo]):
             gc = GithubOrgClient(org_name)
-            repos = gc.public_repos()
-
-            mock_public_repos_url.assert_called_once()
-            mock_get_json.assert_called_once()
-
-            self.assertEqual(repos, expected_repos)
+            result = gc.has_license(license_key)
+            self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
