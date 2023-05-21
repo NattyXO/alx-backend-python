7a210f3e44e89eaa2281a4a2b69a9831641783ba
Natnael Bizuneh <106749223+NattyXO@users.noreply.github.com>
2023-05-21 22:56:32 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,25 +2,24 @@
 """ Unittest module """
 
 from unittest import TestCase, mock
-from unittest.mock import patch, Mock
-from parameterized import parameterized
+from unittest.mock import patch
 
 import client
 from client import GithubOrgClient
 
-
 class TestGithubOrgClient(TestCase):
-    """ Class for testing GithubOrgClient """
+    def test_public_repos_url(self):
+        org_name = "example"
+        payload = {"url": "https://api.github.com/orgs/example"}
 
-    @parameterized.expand([
-        ("google"),
-        ("abc"),
-    ])
-    @patch('client.get_json')
-    def test_org(self, org_name, mock_json):
-        """ Test method returns correct output """
-        gc = GithubOrgClient(org_name)
-        gc.org()
-        mock_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-            )
+        with patch.object(GithubOrgClient, 'org', new_callable=mock.PropertyMock) as mock_org:
+            mock_org.return_value = payload
+
+            gc = GithubOrgClient(org_name)
+            result = gc._public_repos_url
+
+            expected_url = payload["url"]
+            self.assertEqual(result, expected_url)
+
+if __name__ == '__main__':
+    unittest.main()
